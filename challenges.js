1d3b10694a042e6476c19f248124d541268d64ce
Seun Dada <seun.dada777@gmail.com>
2024-04-05 09:28:44 -0400

    solve number 1 and 2

--- challenges.js
+++ challenges.js
@@ -34,9 +34,9 @@ addOne(-5) //=> -4
 -----------------------------------------------------------------------------*/
 // Your solution for 01-addOne here:
 
-
-
-
+function addOne(num) {
+  return num + 1
+}
 
 /*-----------------------------------------------------------------------------
 Challenge: 02-addTwoNumbers
@@ -58,9 +58,13 @@ addTwoNumbers('Hello', 5) //=> NaN
 -----------------------------------------------------------------------------*/
 // Your solution for 02-addTwoNumbers here:
 
-
-
-
+function addTwoNumbers(num1, num2) {
+  if (isNaN(num1) || isNaN(num2)) {
+  return NaN
+  } else {
+  return num1 + num2
+  }
+}
 
 /*-----------------------------------------------------------------------------
 Challenge: 03-sumNumbers
